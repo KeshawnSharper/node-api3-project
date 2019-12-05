1243ee21d55ec4602b4cf3a55458676d737441e1
Lambda_School_Loaner_182 <lambda_school_loaner_182@Lambda-School-Loaner-182s-MacBook-Pro.local>
2019-12-05 12:18:01 -0500

    stretch

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // code away!
+require('dotenv').config();
 const server = require('./server');
 const postRouter = require('./posts/postRouter')
 server.use('/api/posts', postRouter)
+const port = process.env.PORT || 8080
+const onServerStart = (message) => console.log(`${message}`);
 
-const onServerStart = (message) =>console.log(`${message}`);
-
-server.listen(6000, onServerStart('Server running on port 4000 successfully.'));
+server.listen(port, onServerStart('Server running on port 4000 successfully.'));
